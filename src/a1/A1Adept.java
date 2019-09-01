9a630da60e6840d43d8a917928f777a92cb947b6
Andrew Ferro <Andrew Ferro@DEZK>
2019-09-01 17:18:38 -0400

    A1 Adept v1 added min max and average f(x) and takes array values.

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -10,5 +10,76 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		int storeItems = scan.nextInt();
+		String[] itemNameData = new String[storeItems];
+		int[] itemPriceData = new int[storeItems];
+		for (int i=0; i<storeItems; i++) {
+			itemNameData[i] = scan.next();
+			itemPriceData[i] = scan.nextInt();
+		}
+		int customernum = scan.nextInt();
+			double max = 0;
+			double min = 100.00;
+			String[] firstData = new String[customernum];
+			String[] lastData = new String[customernum];
+			double[] paidforData = new double[customernum];
+			
+			for (int i = 0; i<customernum; i++) {
+								
+				firstData[i] = scan.next();
+				
+				lastData[i] = scan.next();
+				
+				int quantity = scan.nextInt();
+				
+				double total = 0;
+			
+				for(int z=0; z<quantity; z++) {
+				
+					int itemquantity = scan.nextInt();				
+					
+					String itemname = scan.next();
+					
+					for (int j=0; j<itemNameData.length; j++) {
+						if(itemname == itemNameData[i]) {
+							total += itemPriceData[i]*itemquantity;
+						}
+					}
+				paidforData[i] = total;
+					
+				};
+					
+			}
+			//max
+			int o = 0;
+			for (int k=0; k < paidforData.length; k++) {
+				if (paidforData[k] > max) {
+					max = paidforData[k];
+					
+				}
+				o = k;
+			}
+			System.out.println("Biggest: " + firstData[o] + " " + lastData[o] + " (" + max + ")");
+			
+			//min
+			int l = 0;
+			for (int k=0; k < paidforData.length; k++) {
+				if (paidforData[k] < min) {
+					min = paidforData[k];
+					
+				}
+				l = k;
+			}
+			System.out.println("Smallest: " + firstData[l] + " " + lastData[l] + " (" + min + ")");
+			
+			//Average
+			double average = 0;
+			for (int k=0; k < paidforData.length; k++) {
+				average = (average + paidforData[k])/paidforData.length;
+			}
+			System.out.println("Average: " + average);
+			
+			
+	scan.close();
 	}
 }
