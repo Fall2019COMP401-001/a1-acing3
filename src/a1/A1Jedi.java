cbe4c4e199734e16da9063e63f0201c42adb4129
Andrew Ferro <Andrew Ferro@DEZK>
2019-09-01 20:59:52 -0400

    A1 Jedi Test

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,74 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		//Store Inventory
+		
+		int storeItems = scan.nextInt();
+		
+		String[] itemNameData = new String[storeItems];
+		
+		double[] itemPriceData = new double[storeItems];
+		
+		int[] cusPerItem = new int[storeItems];
+		
+		int[] itemPurch = new int[storeItems];
+		
+		for (int i=0; i<storeItems; i++) {
+			itemNameData[i] = scan.next();
+			itemPriceData[i] = scan.nextDouble();
+		}
+		
+		
+		//# of Customers
+		int customernum = scan.nextInt();
+		
+		//Data for each customer
+		String[] NameOfCustomer = new String[customernum];
+		double[] PriceData = new double[customernum];
+		
+		//iteration for each customer
+			for (int i = 0; i<customernum; i++) {
+				double total = 0.00;				
+				NameOfCustomer[i] = scan.next() + " " + scan.next();
+				
+				//items purchased
+				int quantity = scan.nextInt();
+			
+				//iteration for each item
+				for(int z=0; z<quantity; z++) {
+				
+					//Amount of a specific item purchased
+					int itemquantity = scan.nextInt();				
+					
+					//Name of the specific Item
+					String itemname = scan.next();
+					
+					//iteration for the price of the item
+					for (int j=0; j<itemNameData.length; j++) {
+						if(itemname.equals(itemNameData[j])) {
+							total += itemPriceData[j]*itemquantity;
+							cusPerItem[j] += 1;
+							itemPurch[j] += itemquantity;
+						}
+					}
+					PriceData[i] = total;
+					
+					
+					//System.out.println("Customer Info: " + NameOfCustomer[i] + " " + PriceData[i]);
+					
+				}
+				
+					
+			}
+			
+	for (int i=0; i<storeItems; i++) {
+		if (itemPurch[i] > 0) {
+			System.out.println(cusPerItem[i] + " customers bought " + itemPurch[i] + " " + itemNameData[i]);
+		} else {
+			System.out.println("No customers bought " + itemNameData[i]);
+		}
 	}
+	scan.close();
+	}
+		
 }
